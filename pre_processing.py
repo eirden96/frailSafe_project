fed79082d5de88f9163338e793020fd6ae1582a1
= <=>
2021-11-11 15:48:01 +0200

    Pre-processing code optimization

--- pre_processing.py
+++ pre_processing.py
@@ -11,22 +11,10 @@ import pandas as pd
 
 clinical_data = pd.read_csv('clinical_dataset.csv', delimiter=';')
 
-def convert_to_numeric(original_data = clinical_data, replace_dict = None, isRegex = False):
+def convert_to_numeric(original_data = clinical_data, replace_dict = None, replace_dic_regex = None):
     try: 
-        if replace_dict != None:
-            print(" We have a replace dict")
-            preprocessed_data = original_data.replace(replace_dict, regex = isRegex)
-            return preprocessed_data
-
-        print("No replace dictionary given. Will use pre-defined replacements")
-        preprocessed_data = original_data.replace({'F': 0, 'M': 1})
-        preprocessed_data = preprocessed_data.replace({'Yes': 1, 'No' :0})
-        preprocessed_data = preprocessed_data.replace({r'well': 2, r'moderately': 1, r'poorly': 0}, regex=True)
-        preprocessed_data = preprocessed_data.replace({True: 1, False :0})
-        preprocessed_data = preprocessed_data.replace({'<5 sec': 1, '>5 sec' :0})
-        preprocessed_data = preprocessed_data.replace({'> 5 h per week': 2,'> 2 h and < 5 h per week': 1, '< 2 h per week' :0})
-        preprocessed_data = preprocessed_data.replace({r'5': 5, r'4': 4, r'3': 3, r'2': 2, r'1': 1}, regex=True)
-        preprocessed_data = preprocessed_data.replace({'Frail': 2,'Pre-frail': 1, 'Non frail' :0})
+        preprocessed_data = original_data.replace(replace_dict)
+        preprocessed_data = preprocessed_data.replace(replace_dic_regex, regex = True)
         return preprocessed_data
         
     except Exception as e:
